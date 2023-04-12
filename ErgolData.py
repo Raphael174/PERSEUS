c23bc8e7a21e2b83cec838287d0461d6a819ba4c
grodcas <119622068+grodcas@users.noreply.github.com>
2023-04-12 19:07:28 +0200

    Ergol

---
+++ ErgolData.py
@@ -0,0 +1,31 @@
+#---------------------------------DATA-------------------------------
+muWat=0.001             #Used for water test experiments
+rhoWat=997              #Used for water test experiments
+rhoWatg=1.27            #Used for water test experiments
+sigmaWat=72*10**(-3)    #Used for water test experiments
+
+
+muShellwax=2.69*10**(-3)    #in lbm7ft-s
+sigmaShellwax=17            # in dynes7cm
+rhoShellwax=47.7            # in lbm7ft3
+
+rhoo=1141 #kg/m3 LOX
+muo=451.36*10**(-6)  #Pa s LOX
+sigmao=0.0132 #N/m
+
+rho = 789; #kg/m3--------------------IMPORTANT
+rhog=1.8437; #kg/m^3-----------------IMPORTANT
+
+Cpg = 6380.6; #j/kg/K
+Cpl=2400; #j/kg/K
+Tcomb=3094; #K
+Tevap=351.6; #K
+Tinj =300; #K
+sigmaL = 21*10**-3; #N/m Surface tension Ethanol at 300K---------IMPORTANT
+sigmaHL = 17*10**-3 ; #N/m Surface tension Ethanol at 350K
+muL=10**-3; #Pa·s Dynamic Viscosity at 300K-----------------------IMPORTANT
+DiffKer=1.7*10**(-4) #An equation for the estimation of alcohol-air diffusion coefficients for modelling evaporation losses in fuel systems
+Yko = 0.96 #Mass fraction of the evaporated drop
+Vflight = 138 #Mean speed of the gaz in the chamber   
+
+#COOLPROP implementar.
